872bab8c20ffde31fb64f04bf4aaa3f9547ea251
Rawshanara Eity <rawshanara.eity@gmail.com>
2023-11-08 02:25:15 +0600

    added wishlist buttton functionality

--- src/Pages/AllBlogs/BlogCard.jsx
+++ src/Pages/AllBlogs/BlogCard.jsx
@@ -1,3 +1,5 @@
+import swal from "sweetalert";
+
 const BlogCard = ({ blogs }) => {
   const {
     _id,
@@ -13,6 +15,40 @@ const BlogCard = ({ blogs }) => {
   } = blogs;
 
   // console.log(Object.keys(blogs).join(','))
+  const myblog = {title,
+    image,
+    category,
+    short_description,
+    long_description,
+    date,
+    current_time,
+    author_name,
+    author_img,}
+
+
+  const handleAddToWishlist = () =>{
+       
+
+    fetch('http://localhost:5000/wishlist', {
+        method: 'POST',
+        headers: {
+            'content-type': 'application/json'
+        },
+        body: JSON.stringify(myblog)
+        
+    })
+    .then(res => res.json())
+    .then(data =>{
+        console.log(data)
+        if(data.acknowledged){
+            swal("Congrats","Add Your Wishlist Successfully", "success");
+          
+        }
+    })
+  }
+
+
+
   return (
     <div>
       <div className="card bg-base-100 shadow-xl">
@@ -28,8 +64,8 @@ const BlogCard = ({ blogs }) => {
           </div>
           <p>{short_description.slice(0,250)}</p>
           <div className="card-actions justify-end">
-            <div className="btn bg-slate-800 text-white rounded-full">Details</div>
-            <div className="btn bg-gradient-to-r from-cyan-500 to-teal-500 text-white rounded-full">Wishlist</div>
+            <button className="btn bg-slate-800 text-white rounded-full">Details</button>
+            <button onClick={handleAddToWishlist} className="btn bg-gradient-to-r from-cyan-500 to-teal-500 text-white rounded-full">Wishlist</button>
           </div>
         </div>
       </div>
